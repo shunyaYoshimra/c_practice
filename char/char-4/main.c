c3c2897ea03df1bfa8048482330a6e7c993eccb9
shunyaYoshimura <maoorgri1015@gmail.com>
2021-10-28 19:04:22 +0900

    record char-4

---
+++ char/char-4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+  char s1[256], s2[256];
+  int a = 100, b = 200;
+  sprintf(s1, "%d", a);
+  sprintf(s2, "bの値は%dです", b);
+  puts(s1);
+  puts(s2);
+}
